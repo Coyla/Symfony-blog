0a5a9e4c3cdd1666ff531c00314962908744b46c
Juan Coyla <juan.coyla@estiam.com>
2017-11-19 18:25:43 +0100

    Add template admin section index

---
+++ src/DEV/BlogBundle/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DEV\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     * @Method({"GET"})
+     */
+    public function indexAction(){
+        $test  = '2a$12$cyTWeE9kpq1PjqKFiWUZFuCRPwVyAZwm4XzMZ1qPUFl7/flCM3V0';
+
+        return $this->render
+        ('DEVBlogBundle:Admin:index.html.twig');
+    }
+}
---
+++ src/DEV/BlogBundle/Tests/Controller/AdminControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DEV\BlogBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminControllerTest extends WebTestCase
+{
+}
